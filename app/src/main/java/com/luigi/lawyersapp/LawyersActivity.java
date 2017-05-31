82733df58dc8a84473f7b95d668d11fe3f6a478c
Luis Arrix <luis.arrix@gmail.com>
2017-05-31 12:25:31 -0300

    El resto de la app

---
+++ app/src/main/java/com/luigi/lawyersapp/LawyersActivity.java
@@ -0,0 +1,13 @@
+package com.luigi.lawyersapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LawyersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lawyers);
+    }
+}
